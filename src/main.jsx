71a074851a76ed94a69e26892a7b14c13ea19ed3
MELLO <neo@neoprotocol.space>
2026-02-17 18:03:41 -0300

    chore: update ecosystem roadmap to official organization

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,26 @@
-
 import ReactDOM from 'react-dom/client';
 import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
 import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { createConfig, WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { http } from 'viem';
+import { mainnet } from 'viem/chains';
+
 import App from './App';
 import './index.css';
 
-const dynamicEnvId = import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID
+const dynamicEnvId = import.meta.env.VITE_DYNAMIC_ENVIRONMENT_ID;
+
+const config = createConfig({
+    chains: [mainnet],
+    multiInjectedProviderDiscovery: false,
+    transports: {
+        [mainnet.id]: http(),
+    },
+});
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <DynamicContextProvider
@@ -14,6 +29,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             walletConnectors: [EthereumWalletConnectors],
         }}
     >
-        <App />
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                <DynamicWagmiConnector>
+                    <App />
+                </DynamicWagmiConnector>
+            </QueryClientProvider>
+        </WagmiProvider>
     </DynamicContextProvider>
 );
